95e00c212f1a3ab648ceda9c34d1d67bab986a9d
Gabriel Augusto <gabrielsebe6@gmail.com>
2025-09-13 22:12:32 -0300

    Criando App Transição de Tela

---
+++ app-transicao-tela/components/PerfilScreen.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { View, Text, Button } from "react-native";
+
+function PerfilScreen({ navigation }){
+    return(
+        <View 
+        style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}
+        >
+            <Text>Bem vindo a tela de Perfil</Text>
+            <Button
+                title="Voltar a tela Inicial"
+                onPress={() => navigation.goBack()}
+            />
+        </View>
+    );
+}
+
+export default PerfilScreen;
